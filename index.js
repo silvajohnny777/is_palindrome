603ec6aeb9c687fae51ef3ec3cd5b968bd844241
silvajohnny777 <jonatha_silva2012@hotmail.com>
2019-07-27 12:06:32 -0300

    Palindrome

---
+++ index.js
@@ -0,0 +1,28 @@
+function is_palindrome() {
+	let palavra1 = document.getElementById('palavra1').value;
+    let palavra1_invertida = [];    
+	let palavra1_nospace = palavra1.replace(/ /ig, "").toLowerCase();
+	
+	for (let i = palavra1_nospace.length-1 ; i >= 0 ; i--) {
+        palavra1_invertida.push(palavra1_nospace.charAt(i));
+    }
+	
+	for (let k = 0 ; k < palavra1_nospace.length ; k ++) {
+        if (palavra1_nospace.charAt(k) == palavra1_invertida[k])   {
+            if ( k == palavra1_nospace.length-1 ) {
+                document.getElementById('title').innerHTML = "Yeah! '" +palavra1 +"' is a palindrome :)";   
+            }
+        } else {
+            document.getElementById('title').innerHTML = "Oh... '" +palavra1 +"' is not a palindrome :(";
+            break;
+        }
+    }
+	
+}
+
+// code bellow from web
+function process(input){
+  let value = input.value;
+  let numbers = value.replace(/[0-9]/g, "");
+  input.value = numbers;
+}
