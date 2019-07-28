adbf92c8dc6c8cbee8844a535f9a102375f8f4fe
Jonatha Silva <38920081+silvajohnny777@users.noreply.github.com>
2019-07-28 20:28:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 function is_palindrome() {
-	let palavra1 = document.getElementById('palavra1').value;
-    let palavra1_invertida = [];    
-	let palavra1_nospace = palavra1.replace(/[^a-z0-9]/gi,'').toLowerCase();
-	
-	for (let i = palavra1_nospace.length-1 ; i >= 0 ; i--) {
-        palavra1_invertida.push(palavra1_nospace.charAt(i));
+	let word1 = document.getElementById('word1').value;
+    let word1_inverted = [];    
+    let word1_nospace = word1.replace(/[^a-z0-9]/gi,'').toLowerCase();
+    
+    document.getElementById('word1').onkeypress = function(e) {
+        if(e.keyCode == 13) {
+            alert('You pressed enter!');
+        }
     }
 	
-	for (let k = 0 ; k < palavra1_nospace.length ; k ++) {
-        if (palavra1_nospace.charAt(k) == palavra1_invertida[k])   {
-            if ( k == palavra1_nospace.length-1 ) {
-                document.getElementById('title').innerHTML = "Yeah! '" +palavra1 +"' is a palindrome :)";   
+	for (let i = word1_nospace.length-1 ; i >= 0 ; i--) {
+        word1_inverted.push(word1_nospace.charAt(i));
+    }
+	
+	for (let k = 0 ; k < word1_nospace.length ; k ++) {
+        if (word1_nospace.charAt(k) == word1_inverted[k])   {
+            if ( k == word1_nospace.length-1 ) {
+                document.getElementById('title').innerHTML = "Yeah! '" +word1 +"' is a palindrome :)";   
             }
         } else {
-            document.getElementById('title').innerHTML = "Oh... '" +palavra1 +"' is not a palindrome :(";
+            document.getElementById('title').innerHTML = "Oh... '" +word1 +"' is not a palindrome :(";
             break;
         }
-    }	
+    }
+	
 }
